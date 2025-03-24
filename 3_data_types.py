a2ee0f07ce71fd5e80d91aeb8c4b9b010146ddf0
faqihabdul <faqihabdulmannan1310@gmail.com>
2025-03-24 14:28:08 +0700

    Add examples of different data types in Python
    This file contains examples of different data types in Python, including:
    - Integer (int)
    - Floating point (float)
    - String (str)
    - Boolean (bool)
    - Complex numbers (complex)
    
    Each example prints the data and its type using the type() function.

---
+++ 3_data_types.py
@@ -0,0 +1,27 @@
+# a = 10, a adalah variable dengan nilai
+
+#tipe data:angka satuan yang gada koma(integar)
+data_integer = 1
+print('data: ', data_integer)
+print('-bertipe', type(data_integer))
+
+#tipe data: angka dengan koma (float)
+data_float = 2.5
+print('data: ', data_float)
+print('-bertipe', type(data_float))
+
+#tipe data: kumpulan karakter (string)
+data_string = 'wili dan ucup'
+print('data: ', data_string)
+print('-bertipe', type(data_string))
+
+#tipe data: biner true/false (boolean)
+data_boolean = True
+print('data: ', data_boolean)
+print('-bertipe', type(data_boolean))
+
+##tipe data khusus
+data_complex = complex(5,7)
+print('data: ', data_complex)
+print('-bertipe', type(data_complex))
+
