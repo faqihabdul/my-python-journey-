1afb571c662f3425200ddbbc44258d25e29a4a8e
faqihabdul <faqihabdulmannan1310@gmail.com>
2025-03-24 14:24:16 +0700

    Add basic print and variable assignment examples
    This file contains basic examples of variable assignment and printing values in Python.
    It demonstrates how to assign values to variables, call them, and use indirect assignment.

---
+++ 2_variables_and_assignment.py
@@ -0,0 +1,24 @@
+#variable adalah tempat menyimpan data
+
+#menaruh / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+#pemanggilan pertama
+print("nilai a adalah= ", a)
+print("nilai x adalah= ", x)
+print("nilai panjang adalah= ", panjang)
+
+#penamaan
+nilai_y = 15 #dengan menggunakan underscore
+nilaiZ = 10 #ini boleh
+
+#pemanggilan kedua
+print("nilai a adalah= ", a)
+a = 6
+print("nilai a adalah= ", a)
+
+#assignment indirect
+b = a
+print("nilai b adalah= ", b)
