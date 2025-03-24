850818798b1dfd09b66970296375a985b2a3cac0
faqihabdul <faqihabdulmannan1310@gmail.com>
2025-03-24 14:15:17 +0700

    Update and rename belajar 1.py to 1_basic_print_statements.py

---
+++ 1_basic_print_statements.py
@@ -0,0 +1,22 @@
+# Program ini menunjukkan cara menggunakan print dasar
+# This program demonstrates basic print statements
+
+print("Hello")  # Mencetak teks sederhana / Printing a simple text
+print("world")  # Mencetak teks lainnya / Printing another text
+
+# Mendeklarasikan variabel / Declaring a variable
+a = 10
+
+print("Hello, World!")  # Mencetak pesan sapaan / Printing a greeting message
+print(a)  # Mencetak nilai variabel 'a' / Printing the value of variable 'a'
+
+""" 
+Ini adalah komentar multi-baris 
+Bisa digunakan untuk menjelaskan program lebih detail 
+
+This is a multiline comment 
+It can be used to describe the program in detail 
+"""
+
+
+
--- belajar 1.py
+++
@@ -1,9 +0,0 @@
-print("hello")
-print("dunia")
-
-a = 10
-print("hello world") #ini adalah coment
-print(a)
-"""ini multiline slebew"""
-
-
