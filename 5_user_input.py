93f981e05ee9a9d2d62a83a840bd76d11a0a7cba
faqihabdul <faqihabdulmannan1310@gmail.com>
2025-03-24 14:39:19 +0700

    Add examples of user input handling in Python
    This file contains examples of handling user input in Python.
    By default, input() returns a string, but it can be converted to an integer using int().

---
+++ 5_user_input.py
@@ -0,0 +1,17 @@
+#data yang dimasukan pasti string
+data = input("masukan data ")
+print('data= ', data, 'tipe= ', type(data))
+
+#jika kita ingin mengambil int, maka
+number = int(input("masukan angka"))
+print('data= ', number, 'tipe= ', type(number))
+
+biner = bool(int(input("masukan nilai")))
+
+print('data= ', biner, 'tipe= ', type(biner))
+
+
+
+
+
+
